f5514b215fa56a17d680856679e7b879fdb002e0
David Czeck <david@ibiblio.org>
2016-09-07 23:34:21 -0700

    Demo set-up

---
+++ demo/systemjs.config.js
@@ -0,0 +1,52 @@
+(function(global) {
+
+  var ngVer = '@2.0.0-rc.6';
+
+  // map tells the System loader where to look for things
+  var map = {
+    'app':  'app', // 'dist',
+    'rxjs': 'https://unpkg.com/rxjs@5.0.0-beta.11',
+    '@angular': 'https://unpkg.com/@angular'
+  };
+
+  // packages tells the System loader how to load when no filename and/or no extension
+  var packages = {
+    'app':  { main: 'main.ts',  defaultExtension: 'ts' },
+    'rxjs': { defaultExtension: 'js' }
+  };
+
+  var ngPackageNames = [
+    'common',
+    'compiler',
+    'core',
+    'forms',
+    'platform-browser',
+    'platform-browser-dynamic'
+  ];
+
+  ngPackageNames.forEach(function(pkgName) {
+    map['@angular/'+pkgName] = 'https://unpkg.com/@angular/' + pkgName + ngVer;
+  });
+
+  // Add package entries for angular packages
+  ngPackageNames.forEach(function(pkgName) {
+    packages['@angular/'+pkgName] = { main: '/bundles/' + pkgName + '.umd.js', defaultExtension: 'js' };
+  });
+
+  var config = {
+    transpiler: 'typescript',
+    typescriptOptions: {
+      emitDecoratorMetadata: true
+    },
+    map: map,
+    packages: packages
+  }
+
+  // filterSystemConfig - index.html's chance to modify config before it is registered.
+  if (global.filterSystemConfig) {
+    global.filterSystemConfig(config);
+  }
+
+  System.config(config);
+
+})(this);
