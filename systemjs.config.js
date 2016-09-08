8c5bbfcd52c96285d95b899cfabf42d47c0f408c
David Czeck <david@ibiblio.org>
2016-09-07 18:19:56 -0700

    Initial

---
+++ systemjs.config.js
@@ -0,0 +1,53 @@
+/**
+ * System configuration for Angular 2 samples
+ * Adjust as necessary for your application needs.
+ * Override at the last minute with global.filterSystemConfig (as plunkers do)
+ */
+(function(global) {
+
+  // map tells the System loader where to look for things
+  var map = {
+    'app':                        'app', // 'dist',
+    'rxjs':                       'node_modules/rxjs',
+    '@angular':                   'node_modules/@angular'
+  };
+
+  // packages tells the System loader how to load when no filename and/or no extension
+  var packages = {
+    'app':   { main: 'main.js',  defaultExtension: 'js' },
+    'rxjs':  { defaultExtension: 'js' }
+  };
+
+  var ngPackageNames = [
+    'common',
+    'compiler',
+    'core',
+    'forms',
+    'platform-browser',
+    'platform-browser-dynamic'
+  ];
+
+  // Individual files (~300 requests):
+  function packIndex(pkgName) {
+    packages['@angular/'+pkgName] = { main: 'index.js', defaultExtension: 'js' };
+  }
+
+  // Bundled (~40 requests):
+  function packUmd(pkgName) {
+    packages['@angular/'+pkgName] = { main: '/bundles/' + pkgName + '.umd.js', defaultExtension: 'js' };
+  }
+
+  // Most environments should use UMD; some (Karma) need the individual index files
+  var setPackageConfig = System.packageWithIndex ? packIndex : packUmd;
+
+  // Add package entries for angular packages
+  ngPackageNames.forEach(setPackageConfig);
+
+  var config = {
+    map: map,
+    packages: packages
+  };
+
+  System.config(config);
+
+})(this);
